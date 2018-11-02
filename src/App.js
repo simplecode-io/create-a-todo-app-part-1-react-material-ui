b784929174fe8620627a5fcd438c9de856d1f2fe
owen <owen@cloudgiant.ca>
2018-11-02 19:07:25 -0400

    imported TodoList into app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import TodoList from "./components/TodoList";
+
 class App extends Component {
   render() {
-    return <div className="App">{/* insert component */}</div>;
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    );
   }
 }
 
